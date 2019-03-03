5b10f4632716c45abbe691a573fe30fd1191bcac
Atul Dwivedi <atul16dwivedi@gmail.com>
2019-03-04 00:09:44 +0530

    initial commit

---
+++ src/main/java/com/quickcart/offer/componentoffer/OfferController.java
@@ -0,0 +1,17 @@
+package com.quickcart.offer.componentoffer;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OfferController {
+
+    @Value("${offer-new-year}")
+    private String offer;
+
+    @GetMapping(value = "/new-year-offer")
+    public String newYearOffer(){
+        return offer;
+    }
+}
