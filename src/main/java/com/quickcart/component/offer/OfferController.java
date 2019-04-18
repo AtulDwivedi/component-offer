947e4290b9823079c16be1c34c92679ae80c90be
Code WellnWill <code.wnw@gmail.com>
2019-04-18 23:38:02 +0530

    updated configuration

--- src/main/java/com/quickcart/component/offer/OfferController.java
+++ src/main/java/com/quickcart/component/offer/OfferController.java
@@ -1,4 +1,4 @@
-package com.quickcart.offer.componentoffer;
+package com.quickcart.component.offer;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
